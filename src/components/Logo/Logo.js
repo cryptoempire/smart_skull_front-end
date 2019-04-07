b548454326a1963f8f9ca10ac75970d35c21b405
cryptoempire <ecehood@gmail.com>
2019-04-07 15:30:57 -0500

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Tilt from 'react-tilt';
+import './Logo.css';
+import skull from './skull.png';
+
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+    <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+      <div className="Tilt-inner pa4"> <img src={skull} alt='skull logo' /> </div>
+    </Tilt>
+
+    </div>);
+};
+export default Logo;
